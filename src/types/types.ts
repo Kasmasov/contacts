b342a50c5dd5bbaf2f3112c2499886c526d15541
Valerii Kasmasov <103592082+Kasmasov@users.noreply.github.com>
2022-09-30 22:30:17 +0300

    correct the types

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,20 +1,25 @@
+
 export type TAdress = {
     street: string;
     city: string;
     zipcode: string;
     suite: string;
-}
+};
 
 export type TCard = {
+    id: number;
     firstName: string;
     lastName: string;
     phone: string;
     email: string;
     note: string;
+    company: string;
     adress: TAdress;
-}
+};
 
-export type TList = {
+export type TItemList = {
     firstName: string;
     lastName: string;
-}
+};
+
+export type TList = TItemList[];
