9830c0f17cd839112fabdc72b02602ad85dca2d2
Valerii Kasmasov <103592082+Kasmasov@users.noreply.github.com>
2022-10-09 21:32:02 +0300

    just save

--- src/component/ContactList.tsx
+++ src/component/ContactList.tsx
@@ -5,12 +5,9 @@ import './ContactForm.css'
 
 export const ContactList:FC = () => {
     const { Search } = Input;
-
     const onSearch = (value: string) => console.log(value);
-
     const contactList = useAppSelector(state => state.contacts);
-
-
+      
   return (
     <div className='container-contact-list'>
         <Search 
@@ -25,8 +22,17 @@ export const ContactList:FC = () => {
               height: '100vh',
               margin: '2rem 1rem 0rem 1rem'}} 
               >
-            {contactList.map(item => <h5 key={item.id}>{`${item.lastName} ${item.firstName}`}</h5>)}
-            
+            {contactList.length > 0 
+            ?
+            contactList
+              .map(item => <h5 key={item.id}>{`${item.lastName} ${item.firstName}`}</h5>)
+            : 
+            <h4
+              style={{
+                color:'grey',
+                textAlign:'center'
+              }}
+            > список пока пуст</h4>}
 
         </div>
     </div>
--- src/store/reducers/contactSlice.ts
+++ src/store/reducers/contactSlice.ts
@@ -6,6 +6,8 @@ import { PayloadAction } from '@reduxjs/toolkit'
 const initialState: TContacts = [
     { id: 1, firstName: 'Valerii', lastName: 'Kasmasov' },
     { id: 2, firstName: 'Vadim', lastName: 'Zhukov' },
+    { id: 3, firstName: 'Igor', lastName: 'Abramov' },
+    { id: 4, firstName: 'Igor' },
 ]
 
 export const contacts = createSlice({
